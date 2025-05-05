fc9e239e4ac29825486b61a4bd6c307c875a5726
Sahar Javadi <sahar.javadi.h@gmail.com>
2025-05-05 10:40:29 +0330

    edit functions related to coockies

--- app/utils/functions.js
+++ app/utils/functions.js
@@ -58,7 +58,7 @@ async function setAccessToken(res, user) {
     maxAge: 1000 * 60 * 60 * 24 * 1, // would expire after 1 days
     httpOnly: true, // The cookie only accessible by the web server
     signed: true, // Indicates if the cookie should be signed
-    sameSite: 'none',
+    sameSite: "None",
     secure: process.env.NODE_ENV === "development" ? false : true,
     domain: process.env.DOMAIN,
   };
@@ -74,7 +74,7 @@ async function setRefreshToken(res, user) {
     maxAge: 1000 * 60 * 60 * 24 * 365, // would expire after 1 year
     httpOnly: true, // The cookie only accessible by the web server
     signed: true, // Indicates if the cookie should be signed
-    sameSite: 'none',
+    sameSite: "None",
     secure: process.env.NODE_ENV === "development" ? false : true,
     domain: process.env.DOMAIN,
   };
